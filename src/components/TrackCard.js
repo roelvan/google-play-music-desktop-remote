0ba2f12cafc63111192c47d39329d584fc4202ae
mmazzarolo <mazzarolomatteo@gmail.com>
2016-04-04 22:23:56 +0200

    Little tweaks

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -1,26 +1,44 @@
-/**
- * @providesModule TrackCard
- */
-import React, { Image, Text, View } from 'react-native'
+/** @providesModule TrackCard */
+import React, { Image, StyleSheet, Text, View } from 'react-native'
 import { getTheme } from 'react-native-material-kit'
 import colors from 'GooglePlayMusicDesktopRemote/src/config/colors'
+import dimensions from 'GooglePlayMusicDesktopRemote/src/config/dimensions'
 
 const theme = getTheme()
 
+const placeholder = 'http://media.tumblr.com/tumblr_mf3r1eERKE1qgcb9y.jpg'
+
 export default ({ title, artist, album, albumArt }) =>
-  <View style={[theme.cardStyle, { flex: 1, elevation: 2, margin: 40,
-  overflow: 'visible' }]}>
+  <View style={[theme.cardStyle, styles.container]}>
     <Image
-      source={{ uri: albumArt === '' ? 'http://media.tumblr.com/tumblr_mf3r1eERKE1qgcb9y.jpg' : albumArt }}
-      resizeMode='contain'
-      style={{ flex: 1 }}
+      source={{ uri: albumArt === '' ? placeholder : albumArt }}
+      resizeMode='cover'
+      style={{ flex: 1, margin: -2 }}
     />
-    <View style={{ padding: 15 }}>
-      <Text style={[theme.cardContentStyle, { padding: 0, color: colors.BLACK, fontSize: 16 }]}>
+    <View style={{ margin: 15 }}>
+      <Text style={[theme.cardContentStyle, styles.titleText]}>
         {title}
       </Text>
-      <Text style={[theme.cardContentStyle, { padding: 0 }]}>
+      <Text style={[theme.cardContentStyle, styles.subText]}>
         {artist} - {album}
       </Text>
     </View>
   </View>
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 0,
+    flex: 1,
+    elevation: 2,
+    marginVertical: 40,
+    width: dimensions.DEVICE_WIDTH * 0.7
+  },
+  titleText: {
+    padding: 0,
+    color: colors.BLACK,
+    fontSize: 16
+  },
+  subText: {
+    padding: 0
+  }
+})
